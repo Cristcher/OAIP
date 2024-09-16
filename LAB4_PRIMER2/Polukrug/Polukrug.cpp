ff65fe6d94e1d869d12f638c163074d85a5ba0b3
Cristcher <rutkovskijnikita6@gmail.com>
2024-09-17 00:40:38 +0300

    Add files via upload

---
+++ LAB4_PRIMER2/Polukrug/Polukrug.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+void drawSemiCircle(int radius, char fillChar) {
+    for (int y = radius; y >= 0; --y) {
+        for (int x = -2 * radius; x <= 2 * radius; ++x) {
+            if (x * x + 4 * y * y <= 4 * radius * radius) {
+                cout << fillChar;
+            }
+            else {
+                cout << ' ';
+            }
+        }
+        cout << endl;
+    }
+}
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int radius; char fillChar;
+    cout << "Введите радиус полукруга: ";
+    cin >> radius;
+    cout << "Введите символ для заполнения полукруга: ";
+    cin >> fillChar;
+    drawSemiCircle(radius, fillChar);
+}
---
+++ LAB4_PRIMER2/SmenaZnPeremennyh/SmenaZnPeremennyh.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b; //, c;//
+	cout << "Vvedi peremennuyu a = " << endl;
+	cin >> a;
+	cout << "Vvedi b = " << endl;
+	cin >> b;
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	//Dop peremennaya:
+	//c = a;
+	//a = b;
+	//b = c;
+	cout << "Смененные значения двух переменных" << endl;
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	return 0;
+}
